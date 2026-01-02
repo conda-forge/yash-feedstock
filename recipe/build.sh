69d6daddecc4bd49ffd9e9c9e0e6b487435b7d89
UENO, M. <eunosium-1128@live.jp>
2026-01-02 16:40:05 +0000

    Remove unused Python sysconfig data copying from build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,6 @@ fi
 export A2XFLAGS="--no-xmllint -v --xsltproc-opts=\"--maxdepth 1000000\" \
     --xsl-file=${BUILD_PREFIX}/share/docbook-xsl/manpages/docbook.xsl"
 
-for f in "${BUILD_PREFIX}/lib/python2.7"/_sysconfigdata_*_conda*_linux_gnu.py; do
-    arch=${HOST%%-*}
-    cp -p "$f" "${BUILD_PREFIX}/lib/python2.7/_sysconfigdata_${arch}_conda_linux_gnu.py"
-    break
-done
-
 ./configure --prefix=${PREFIX}
 
 pushd po
